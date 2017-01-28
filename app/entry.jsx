8030f7929f8403755a3b702b514d74d5db64b442
shinsukeimai <imai@viz.media.kyoto-u.ac.jp>
2017-01-28 14:48:42 +0900

    error track

--- app/entry.jsx
+++ app/entry.jsx
@@ -1,4 +1,2 @@
 import './initialize'
 import './root'
-
-throw new Error('ga test throw error')
--- app/google-analytics.jsx
+++ app/google-analytics.jsx
@@ -2,9 +2,18 @@ import ReactGA from 'react-ga'
 
 import {googleAnalyticsId} from './constants'
 
-ReactGA.initialize(googleAnalyticsId)
+ReactGA.initialize(googleAnalyticsId, {
+  debug: true
+})
 
 export const logPageView = () => {
   ReactGA.set({page: window.location.pathname})
   ReactGA.pageview(window.location.pathname)
+}
+
+window.onerror = function (msg, url, line) {
+  ReactGA.exception({
+    description: `${msg} | ${url} | ${line}`,
+    fatal: false
+  })
 }
