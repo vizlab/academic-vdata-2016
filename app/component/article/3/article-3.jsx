f56c1c351e8ad36f5aa4965ef4b93ca2dd9b9242
shinsukeimai <imai@viz.media.kyoto-u.ac.jp>
2017-02-01 16:38:53 +0900

    update

--- app/component/article/3/article-3.jsx
+++ app/component/article/3/article-3.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 
 import {Pagination} from '../pagination'
+import {ArticleHeader} from '../article-header'
+import {Footer} from '../../footer/footer'
 
 export class Article3 extends React.Component {
   render () {
     return (
       <div>
-        article 3
-        <a target='_blank' href='https://academic-vdata-2016.firebaseapp.com/contents/3/'>todo</a>
-        <Pagination currentPage={3} />
+        <ArticleHeader />
+        <div className='container text-center'>
+          <Pagination currentPage={3} />
+          <div>
+            <a href='/articles'>- home -</a>
+          </div>
+        </div>
+        <Footer />
       </div>
     )
   }
