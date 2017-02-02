05bf9f0d7cde07dc09d1e727f6e131a38e25eaa0
shinsukeimai <imai@viz.media.kyoto-u.ac.jp>
2017-02-02 15:26:20 +0900

    update card pic

--- app/component/article/3/article-3-card.jsx
+++ app/component/article/3/article-3-card.jsx
@@ -7,7 +7,7 @@ export const Article3Card = () => {
   return (
     <ArticleCard
       title={title}
-      imgPath='/assets/img/login.png'
+      imgPath='/assets/img/article-3-preview.png'
       text='ネットワーク分析手法の一つである媒介中心性を用い、複数分野の媒介となる研究者（または研究分野）を分析。異分野連携のキーが可視化される！'
       articlePath='/articles/3'
     />
--- app/component/article/5/article-5-card.jsx
+++ app/component/article/5/article-5-card.jsx
@@ -7,7 +7,7 @@ export const Article5Card = () => {
   return (
     <ArticleCard
       title={title}
-      imgPath='/assets/img/login.png'
+      imgPath='/assets/img/article-5-preview.jpeg'
       text='本調査で回答いただいた100項目のアンケート結果から、研究の価値観や行動様式を分析して学術分野の文化を抽象化し見える化する！　意外な分野が相性がよい？'
       articlePath='/articles/5'
     />
