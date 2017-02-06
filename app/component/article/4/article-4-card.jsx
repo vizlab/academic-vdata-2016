e7b06f67fbf1527ddc1ae0bc7b0f54a63a90f6ab
Shinsuke IMAI <imaishinsuke@gmail.com>
2017-02-06 13:45:33 +0900

    change img

--- app/component/article/4/article-4-card.jsx
+++ app/component/article/4/article-4-card.jsx
@@ -7,7 +7,7 @@ export const Article4Card = () => {
   return (
     <ArticleCard
       title={title}
-      imgPath='/assets/img/article-4-preview.jpeg'
+      imgPath='/assets/img/article-4-preview.jpg'
       text='例えば，「研究の主たる成果は論文よりも著書のほうが価値がある」という回答に同意か否かの回答を研究者ネットワークMAPに重ねる．これにより，研究分野ごとの文化的特性が詳らかに！'
       articlePath='/articles/4'
     />
